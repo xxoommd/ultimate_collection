8233be2f54f867e9260d0ff32a4d1b5f99d33daa
gaohengyi <xxoommd@163.com>
2021-11-09 18:07:13 +0800

    update ss

--- shadowsocks/init_ss_for_ubuntu_1804.sh
+++ shadowsocks/init_ss_for_ubuntu_1804.sh
@@ -12,6 +12,13 @@ sudo sysctl -p; lsmod |grep bbr
 echo "Installing docker..."
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 usrname=`whoami` && sudo usermod -aG docker $usrname
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://19z8a5pk.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
 sudo systemctl enable docker
 sudo systemctl start docker
 # docker run -dt --restart always --name ssserver -p 9527:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k fuckyouall" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
