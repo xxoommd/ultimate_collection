cf72b781dd77817fac159e02f14755aaa2ee9d30
gaohengyi <xxoommd@163.com>
2022-12-25 00:26:13 +0800

    Add naiveproxy

---
+++ naive/install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cd ~ && wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz && rm -f go1.19.4.linux-amd64.tar.gz && echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile && source /etc/profile && go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest && ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
