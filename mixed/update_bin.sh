7faca7c81f7882380430d75b694b2f8ff664c9e0
gaofei <xxoommd@163.com>
2024-08-16 17:52:46 +0800

    update caddy

--- mixed/update_bin.sh
+++ mixed/update_bin.sh
@@ -7,21 +7,55 @@ YELLOW='\033[0;33m'
 UNDERLINE='\033[4m' # 下划线
 NC='\033[0m'        # No Color
 
+function install_go() {
+  wget "https://go.dev/dl/$(curl https://go.dev/VERSION?m=text | grep go).linux-amd64.tar.gz" &&
+    sudo tar -xf go*.linux-amd64.tar.gz -C /usr/local/ &&
+    rm go*.linux-amd64.tar.gz &&
+    echo 'export GOROOT=/usr/local/go' >>~/.profile &&
+    echo 'export PATH=$GOROOT/bin:$PATH' >>~/.profile &&
+    echo 'export GO111MODULE=on' >>~/.profile &&
+    source ~/.profile
+}
+
+if command -v go >/dev/null 2>&1; then
+  echo "Go is installed"
+  go version
+else
+  echo "Go is not installed"
+  install_go
+
+  if [[ $? != 0 ]]; then
+    echo "Install go fail"
+    exit 1
+  fi
+fi
+
+function update_caddy() {
+  go env -w GO111MODULE=on &&
+    go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest &&
+    ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive &&
+    chmod +x ./caddy
+}
+
+function update_hysteria() {
+  wget -O ./hysteria https://download.hysteria.network/app/latest/hysteria-linux-amd64-avx && chmod +x ./hysteria
+}
+
 echo
 echo -e "[INFO] Updating ${GREEN}caddy${NC} ..."
-
-wget "https://go.dev/dl/$(curl https://go.dev/VERSION?m=text | grep go).linux-amd64.tar.gz" &&
-  tar -xf go*.linux-amd64.tar.gz -C /usr/local/ &&
-  echo 'export GOROOT=/usr/local/go' >>/etc/profile &&
-  echo 'export PATH=$GOROOT/bin:$PATH' >>/etc/profile &&
-  echo 'export GO111MODULE=on' >>/etc/profile &&
-  source /etc/profile &&
-  go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest &&
-  ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
-
-echo -e "[INFO] Updating ${GREEN}caddy${NC} success"
-
+if update_caddy; then
+  echo -e "[INFO] Updating ${GREEN}caddy${NC} success"
+else
+  echo -e "[INFO] Updating ${GREEN}caddy${NC} fail"
+  exit 1
+fi
 echo
+
 echo -e "[INFO] Updating ${GREEN}hysteria2${NC} ..."
-echo -e "[INFO] Updating ${GREEN}hysteria2${NC} success"
+if update_hysteria; then
+  echo -e "[INFO] Updating ${GREEN}hysteria2${NC} success"
+else
+  echo -e "[INFO] Updating ${GREEN}hysteria2${NC} fail"
+  exit 1
+fi
 echo
